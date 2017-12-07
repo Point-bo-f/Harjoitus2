51a977ea32884f1b82d19e5dacaaf5da175e52d7
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2017-12-07 20:10:43 +0200

    Liitetty lämmönasennus

--- Harjoitus2/Harjoitus2/Program.cs
+++ Harjoitus2/Harjoitus2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace Harjoitus2
     {
         static void Main(string[] args)
         {
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            string uusiTieto = @"C:\Temp\UusiLämpötila2.txt";
+
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("aiempi lämpötila oli" + aiempiArvo + " astetta");
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa!");
+            }
+            int lämpotila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            lämpotila = int.Parse(syöte);
+
+            // Tiedostoon kirjoittaminen
+
+            File.WriteAllText(tiedosto, lämpotila.ToString());
+            File.Copy(tiedosto, uusiTieto);
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            Console.ReadLine();
         }
     }
 }
